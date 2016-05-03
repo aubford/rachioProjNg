37e3055857d0c9985673882e5bf7183ae2c7ba55
Aubrey Ford <musicaubrey@gmail.com>
2016-05-03 01:59:45 -0600

    getting device info

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -1,25 +1,17 @@
-app.controller('IndexController', ['$scope', 'getUserInfo', 'durations', 'userId', function($scope, getUserInfo, durations, userId){
+app.controller('IndexController', ['$scope', 'getUserInfo', 'durations', 'userInfo',function($scope, getUserInfo, durations, userInfo){
 
-     getUserInfo.getId('c3667b81-92a6-4913-b83c-64cc713cbc1e').then(function(results){
-         console.log(results)
-         userId.id = results.data.id
-     })
+    userInfo.token = 'c3667b81-92a6-4913-b83c-64cc713cbc1e'
 
-     $scope.durations  = durations
+    getUserInfo.getId().then(function(results){
+        //  console.log(results)
+         userInfo.id = results.data.id
 
+         getUserInfo.getUserHardwareInfo(userInfo.id).then(function(results){
+           console.log(results);
+         })
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
+    $scope.durations  = durations
 
 
 
@@ -27,7 +19,7 @@ app.controller('IndexController', ['$scope', 'getUserInfo', 'durations', 'userId
 
     $scope.logValue = function(value){
       console.log(value)
-      console.log(userId.id)
+      console.log(userInfo.id)
     }
 
     }])
--- app/public/js/services.js
+++ app/public/js/services.js
@@ -1,12 +1,16 @@
-app.factory('getUserInfo', ['$http','userId',function($http, userId){
+app.factory('getUserInfo', ['$http','userInfo', function($http, userInfo){
   return {
-    getId: function(token){
-      return $http.get('https://api.rach.io/1/public/person/info', {headers: {'Authorization' : 'Bearer ' + token}
-    })
-
+    getId: function(){
+      return $http.get('https://api.rach.io/1/public/person/info', {headers: {'Authorization' : 'Bearer ' + userInfo.token}
+      })
+    },
+    getUserHardwareInfo: function(id){
+      return $http.get('https://api.rach.io/1/public/person/'+id, {headers: {'Authorization' : 'Bearer ' + userInfo.token}
+      })
     }
   }
 }])
 
-app.value('userId', {id: 'wrong'})
+app.value('userInfo', {id: '',
+token: ''})
 app.value('durations', [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,120,180,240,300,360,420,480,540,600])
